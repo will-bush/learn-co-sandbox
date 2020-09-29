96bf4dccbf17232acbefc21574c9ea1fa82a47b0
Will Bush <williamdcbush@gmail.com>
2020-09-29 09:49:22 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,6 @@
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+ 
+greeting_programmer("Maria", "Ruby")
+greeting_programmer("Steven", "Elixir")
